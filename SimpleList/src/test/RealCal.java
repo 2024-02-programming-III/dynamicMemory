2d18faa8a0e087ccb06293d2ff05a3a2fb73bf2c
IGabrielMT <iangmartinezt@gmail.com>
2024-09-03 11:10:26 -0500

    SOMETHING CHANGED

--- SimpleList/src/test/RealCal.java
+++ SimpleList/src/test/RealCal.java
@@ -8,7 +8,7 @@ public class RealCal<T> {
     
    
     public void run(List<T> list, String title) {
-        Calculator<T> cal = new Calculator<T>(list);
+        Calculator<T> cal = new Calculator<>(list);
         System.out.println();
         System.out.println("----------"+ title +"----------");
         int result;
